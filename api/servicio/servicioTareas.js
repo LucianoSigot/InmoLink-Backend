e5235a15481a2e219ad6982c3cefb3cdd3a22e06
Luciano Sigot <lucianosigot@gmail.com>
2025-11-18 02:47:06 -0300

    Delete api directory

--- api/servicio/servicioTareas.js
+++
@@ -1,7 +0,0 @@
-//la idea es la creacion de la lista de tareas en mongo db
-import Titulo from "../../../FrontEnd/src/componentes/atomos/Titulo.jsx";
-import ListaDeTareas from "../listaDeTareas";
-
-export function servicioTareas() {
-       
-}
